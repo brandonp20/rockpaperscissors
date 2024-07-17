2024d8d4bf1bb1bd1ef902290efd5b7b648ab116
Brandon <brandon.payne20@gmail.com>
2024-07-16 20:25:53 -0700

    Updated

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let playerChoice;
-let computerChoice; 
+let playerChoice, computerChoice;
+let playerScore, computerScore = 0; 
 
-getPlayerChoice = prompt("Rock, Paper, or Scissors?")
-console.log(getPlayerChoice);
+function getPlayerChoice() {
+    playerChoice = prompt("Rock, Paper, or Scissors?")
+    playerChoice = playerChoice.toLowerCase();
+    console.log(getPlayerChoice);
+}
 
 function getComputerChoice() {
     let compDecider = Math.random()
@@ -16,4 +19,31 @@ function getComputerChoice() {
     console.log(computerChoice);
 }
 
+function determineWinner() {
+    if (computerChoice === 'paper' && playerChoice ==='rock') {
+        computerScore =  + 1;
+        console.log("Computer Wins!");
+    } else if (computerChoice === 'paper' && playerChoice ==='scissors') {
+        playerScore =  + 1;
+        console.log("Player Wins!");
+    } else if (computerChoice === 'rock' && playerChoice ==='paper') {
+        playerScore =  + 1;
+        console.log("Player Wins!");
+    } else if (computerChoice === 'rock' && playerChoice ==='scissors') {
+        computerScore =  + 1;
+        console.log("Computer Wins!");
+    } else if (computerChoice === 'scissors' && playerChoice ==='paper') {
+        computerScore =  + 1;
+        console.log("Computer Wins!");
+    } else if (computerChoice === 'scissors' && playerChoice ==='rock') {
+        playerScore =  + 1;
+        console.log("Player Wins!");
+    } else {
+        console.log("It's a tie!");
+    }
+    console.log("Player Score ${playerScore}");
+}
+
+getPlayerChoice()
 getComputerChoice();
+determineWinner();
